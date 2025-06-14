8086081177d16f2e4df3f79667fa3ef340a41497
Benjamin Wöster <benjamin.woester@gmail.com>
2025-06-14 12:44:23 +0200

    provide access to functions

--- packages/hive-pics-frontend/src/firebase/index.ts
+++ packages/hive-pics-frontend/src/firebase/index.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getFunctions } from 'firebase/functions'
 import { getStorage } from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore(app)
+const functions = getFunctions(app)
 const storage = getStorage(app)
 
-export { auth, db, storage }
+export { auth, db, functions, storage }
