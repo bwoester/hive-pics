3da2e5544930647dbeabcf7e23396e5afa1bc855
Benjamin Wöster <benjamin.woester@gmail.com>
2025-06-17 14:22:52 +0200

    exclude formatting from linter run

--- hive-pics-frontend/eslint.config.mjs
+++ hive-pics-frontend/eslint.config.mjs
@@ -1,3 +1,16 @@
+import eslintConfigPrettierFlat from "eslint-config-prettier/flat";
 import vuetify from "eslint-config-vuetify";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
-export default vuetify();
+export default vuetify(
+  {},
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      // Turn off formatting rules via eslint-config-prettier
+      ...eslintConfigPrettierFlat.rules,
+    },
+  },
+);
