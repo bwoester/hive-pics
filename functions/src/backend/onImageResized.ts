adcd537b85405611c43363a59f07a35622f3b118
Benjamin Wöster <benjamin.woester@gmail.com>
2025-07-01 19:50:50 +0200

    actually implement the image resize success callback

--- functions/src/backend/onImageResized.ts
+++ functions/src/backend/onImageResized.ts
@@ -2,20 +2,12 @@ import { onCustomEventPublished } from "firebase-functions/v2/eventarc";
 import * as z from "zod/v4-mini";
 import { HttpsError } from "firebase-functions/https";
 import { getFirestore } from "firebase-admin/firestore";
-import { getStorage } from "firebase-admin/storage";
-import { ChallengePhoto } from "../shared/types";
 
 const ImageResizedEvent = z.object({
   data: z.object({
     // input is an ObjectMetadata from "firebase-functions/v1/storage"
     input: z.object({
-      bucket: z.string(),
       name: z.string(),
-      contentType: z.string(),
-      metageneration: z.string(),
-      updated: z.string(),
-      timeCreated: z.string(),
-      storageClass: z.string(),
       size: z.string(),
       md5Hash: z.string(),
     }),
@@ -38,50 +30,58 @@ export const onImageResized = onCustomEventPublished(
     region: "europe-west4",
   },
   async (event) => {
-      const validation = ImageResizedEvent.safeParse(event);
-      if (!validation.success) {
-          throw new HttpsError("invalid-argument", validation.error.message);
-      } else {
-          const imageResizedEvent = validation.data;
-          // full file path in the bucket (absolute path, file name, file ext)
-          const originalImageFilePath = imageResizedEvent.data.input.name;
-          // full path in firestore (same as the file path in bucket, but file ext removed)
-          const docPath = originalImageFilePath.substring(0, originalImageFilePath.lastIndexOf('.'));
-          const db = getFirestore();
-          const docRef = db.doc(docPath);
+    const validation = ImageResizedEvent.safeParse(event);
+    if (!validation.success) {
+      console.error(validation.error.message);
+      throw new HttpsError("invalid-argument", validation.error.message);
+    } else {
+      const imageResizedEvent = validation.data;
+      // full file path in the bucket (absolute path, file name, file ext)
+      const originalImageFilePath = imageResizedEvent.data.input.name;
+      // full path in firestore (same as the file path in bucket, but file ext removed)
+      const docPath = originalImageFilePath.substring(
+        0,
+        originalImageFilePath.lastIndexOf("."),
+      );
+      const db = getFirestore();
+      const docRef = db.doc(docPath);
 
-          // Check if the document exists
-          const doc = await docRef.get();
-          if (!doc.exists) {
-              console.log(`Document at path ${docPath} does not exist.`);
-              return;
-          }
+      // Check if the document exists
+      const doc = await docRef.get();
+      if (!doc.exists) {
+        const message = `Document at path ${docPath} does not exist.`;
+        console.error(message);
+        throw new HttpsError("not-found", message);
+      }
 
-          const docData = doc.data();
-          if (!docData) {
-              console.log(`Document at path ${docPath} exists but has no data.`);
-              return;
-          }
+      const docData = doc.data();
+      if (!docData) {
+        const message = `Document at path ${docPath} exists but has no data.`;
+        console.error(message);
+        throw new HttpsError("internal", message);
+      }
 
-          // Generate information about resized images
-          const resizedImagesInfo = await Promise.all(
-              imageResizedEvent.data.outputs
-                  .filter(output => output.success)
-                  .map(output_1 => {
-                      return {
-                          size: output_1.size,
-                          storagePath: output_1.outputFilePath,
-                      };
-                  })
-          );
+      // Generate information about resized images
+      const resizedImagesInfo = await Promise.all(
+        imageResizedEvent.data.outputs
+          .filter((i) => i.success)
+          .map((i) => {
+            return {
+              size: i.size,
+              storagePath: i.outputFilePath,
+            };
+          }),
+      );
 
-          if (resizedImagesInfo.length > 0) {
-              await docRef.update({
-                  resizedImages: resizedImagesInfo
-              });
+      if (resizedImagesInfo.length > 0) {
+        await docRef.update({
+          resizedImages: resizedImagesInfo,
+        });
 
-              console.log(`Updated document at path ${docPath} with resized images information.`);
-          }
+        console.info(
+          `Updated document at path ${docPath} with resized images information.`,
+        );
+      }
     }
   },
 );
